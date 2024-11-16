bc52397f4ead61156bd81b92b0a01bacd31c6a4b
Michal Z <michal@MICHPC>
2024-11-16 16:29:14 +0100

    added solar power adjustment

--- backend/src/calculator.py
+++ backend/src/calculator.py
@@ -5,15 +5,19 @@ import matplotlib.pyplot as plt
 possible_cycles = 1500
 battery_price = 20000
 installation_cost = 0
+current_month = "November"
 # PGE
 trade_fee_per_month = 49.90
 trade_fee_per_day = trade_fee_per_month/30
 additional_shit_idont_know = 0
 expected_energy_usage_yearly_kWH = 3000
 expected_daily_energy_usage = expected_energy_usage_yearly_kWH / 365
+number_of_segments_daily = 24
 
 # import daily distribution of power use per hour
 expected_daily_energy_distribution = pd.read_csv("../data/distribution.csv").values
+
+solar_power_monthly = pd.read_csv("../data/SolarPower.csv")
 
 # calculate daily usage of power in kWH per hour
 hours_usage_day_kWH = expected_daily_energy_distribution * expected_daily_energy_usage
@@ -34,17 +38,18 @@ A_kwH = 5 / 1000
 A_month = A_kwH * hours_usage_day_kWH.sum()
 A_day = A_month/30
 
-def calc_energy_price_daily():
-    cost_energy_alone = (hours_usage_day_kWH * hours_cost_day_kWH).sum()
+def calc_energy_price_daily(solar_deduction):
+    cost_energy_alone = ((hours_usage_day_kWH - solar_deduction) * hours_cost_day_kWH).sum()
     return cost_energy_alone + K_day + A_day
 
-def calc_brutto_price_daily():
-    return calc_energy_price_daily() + trade_fee_per_day + additional_shit_idont_know
+def calc_brutto_price_daily(solar_deduction):
+    return calc_energy_price_daily(solar_deduction) + trade_fee_per_day + additional_shit_idont_know
 
 
 
 if __name__ == "__main__":
-    # # battery
+    solar_power_output = solar_power_monthly.loc[solar_power_monthly["Month"] == current_month, "PowerOutput"].values[0]
+
     battery = Battery(
         price=16000, 
         capacity=10, 
@@ -52,9 +57,9 @@ if __name__ == "__main__":
         efficiency=0.9, 
         life_cycles=6000
         )
-    print(f"deposit profit: {battery.calc_deposit_profit(prices.copy())}")
-    print(f"energy cost per day = {round(calc_brutto_price_daily(), 3)}zl")
-    print(f"energy cost if using battery for autoconsumption {battery.calc_battery_autonsumption_cost(prices,expected_daily_energy_usage)}")
+    print(f"deposit profit: {battery.calc_deposit_profit((prices.copy() - solar_power_output )/ number_of_segments_daily)}")
+    print(f"energy cost per day = {round(calc_brutto_price_daily(solar_power_output), 3)}zl")
+    print(f"energy cost if using battery for autoconsumption {battery.calc_battery_autonsumption_cost(prices - solar_power_output / number_of_segments_daily,expected_daily_energy_usage)}")
     # Example inputs
     # prices = np.random.uniform(0.1, 0.5, 96)  # Random energy prices for 96 slots
     # usage = np.random.uniform(0, 2, 96)  # Random energy usage for 96 slots
@@ -80,4 +85,5 @@ if __name__ == "__main__":
     # # plt.show()
     # plt.savefig("plot.png")  # Save the plot to a file
 
+    
 
--- frontend/NodeJSc/nextjs-dashboard/app/dashboard/customers/page.tsx
+++ frontend/NodeJSc/nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image';
+
+
 export default function Page() {
-    return <p>Customers Page</p>;
-  }
+  return <>
+  <Image
+      src="/plot.png"
+      width={1000}
+      height={760}
+      className="hidden md:block"
+      alt="Screenshots of the dashboard project showing desktop version"
+    />
+  </>
+}
+
